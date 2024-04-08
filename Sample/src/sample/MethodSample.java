a6ffa972c8e44a44399820b373ec49bca9c29a51
dir-FY24onoyuto <yuto.ono.t380@dirbato.co.jp>
2024-04-08 11:52:41 +0900

    Exception

---
+++ Sample/src/sample/MethodSample.java
@@ -0,0 +1,33 @@
+package sample;
+
+
+public class MethodSample {
+
+    public static void main(String[]args) {
+        var number1 = 100;
+        var number2 = 10;
+         
+        sumMethod1 (number1,number2);
+        
+        //------追記（ここから）ーーーーーーー
+        var result2 = sumMethod2(number1,number2);
+   System.out.println("sumMethod2の結果は" ＋ result2);
+//---------追記（ここまで）ーーーーーーーー
+    
+    }
+
+    public static void sumMethod1(int num1,int num2) {
+        var result = num1 + num2;
+        System.out.println("sumMethod1の結果は" + result) ;
+        
+        
+        //ーーーーーー追記（ここから）ーーーーーー
+        
+       public static int sumMethod2(int num1,int num2) {
+           var resukt = num1 + num2;
+           return result;           
+       }
+       //ーーーーーーー追記（ここまで）ーーーーーー
+    }
+    
+}
